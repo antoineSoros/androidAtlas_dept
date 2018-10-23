f0838e36f7682b5afde5e5cb254ccad8804791ba
backalight <antoine.soros@orange.fr>
2018-10-23 15:35:27 +0200

    CREATION APPLICATION EN COUCHES

--- app/src/main/java/fr/formation/atlas_dept/MainActivity.java
+++ app/src/main/java/fr/formation/atlas_dept/MainActivity.java
@@ -3,16 +3,30 @@ package fr.formation.atlas_dept;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etSearch,etNoDept,etNom,etNomStd,etSurface,etDateCreation,etChefLieu,etUrlWiki,etNoRegion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etChefLieu=findViewById(R.id.etChefLieu);
+        etDateCreation=findViewById(R.id.etDateCreation);
+        etNoDept=findViewById(R.id.etNoDept);
+        etNom=findViewById(R.id.etNom);
+        etNomStd=findViewById(R.id.etNomStd);
+        etSurface=findViewById(R.id.etSurface);
+        etUrlWiki=findViewById(R.id.etUrlWiki);
+        etNoRegion=findViewById(R.id.etNoRegion);
+
     }
 
     public void btSearch(View view) {
+
     }
 
     public void btClear(View view) {
