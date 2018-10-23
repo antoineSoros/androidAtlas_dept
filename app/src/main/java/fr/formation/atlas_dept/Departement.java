5ba1d008c5fe6982d175955edeec383ae34d96f3
backalight <antoine.soros@orange.fr>
2018-10-23 11:58:04 +0200

    CREATION APPLICATION EN COUCHES

---
+++ app/src/main/java/fr/formation/atlas_dept/Departement.java
@@ -0,0 +1,12 @@
+package fr.formation.atlas_dept;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Departement {
+    private SQLiteDatabase db;
+
+    public Departement(Context c) {
+
+    }
+}
--- app/src/main/java/fr/formation/atlas_dept/MainActivity.java
+++ app/src/main/java/fr/formation/atlas_dept/MainActivity.java
@@ -2,6 +2,7 @@ package fr.formation.atlas_dept;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btSearch(View view) {
+    }
+
+    public void btClear(View view) {
+    }
+
+    public void btDelete(View view) {
+    }
+
+    public void btInsert(View view) {
+    }
+
+    public void btUpdate(View view) {
+    }
+
+    public void deleteDB(View view) {
     }
 }
